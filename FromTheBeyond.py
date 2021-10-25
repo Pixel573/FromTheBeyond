00618c14aef8c56e33e0105c15826c93f6f24f15
iiTreatz <djfhsak7@gmail.com>
2021-10-24 20:33:31 -0400

    Update FromTheBeyond.py

--- FromTheBeyond.py
+++ FromTheBeyond.py
@@ -5,6 +5,11 @@ import pygame
 pygame.init()
 
 # Window management
-pygame.display.set_mode(800, 600)
+pygame.display.set_mode((800, 600))
 pygame.display.set_caption("From The Beyond")
 
+Running = True
+while Running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Running = False
