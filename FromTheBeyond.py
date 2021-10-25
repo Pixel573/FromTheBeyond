33e7e7c200290183cfc33cfe92906a66fc2f6ec2
iiTreatz <djfhsak7@gmail.com>
2021-10-24 20:29:44 -0400

    FromTheBeyond.py
    The main game

---
+++ FromTheBeyond.py
@@ -0,0 +1,10 @@
+# Importing pygame and other dependencies
+import pygame
+
+# Initiate pygame
+pygame.init()
+
+# Window management
+pygame.display.set_mode(800, 600)
+pygame.display.set_caption("From The Beyond")
+
